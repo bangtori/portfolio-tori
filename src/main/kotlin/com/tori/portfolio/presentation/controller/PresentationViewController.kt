5a16f4258687405061a2924d2586a90d502a91f2
Tori <ttang1135@naver.com>
2025-03-17 10:49:53 +0900

    feat: 뷰컨트롤러 제작

--- src/main/kotlin/com/tori/portfolio/presentation/controller/PresentationViewController.kt
+++ src/main/kotlin/com/tori/portfolio/presentation/controller/PresentationViewController.kt
@@ -1,12 +1,38 @@
 package com.tori.portfolio.presentation.controller
 
+import com.tori.portfolio.domain.constant.SkillType
+import com.tori.portfolio.presentation.service.PortfolioService
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class PresentationViewController {
+class PresentationViewController(
+    private val service: PortfolioService
+) {
     @GetMapping("/test")
     fun test(): String {
         return "test"
+    }
+    @GetMapping("/")
+    fun index(model: Model): String {
+        val introductions = service.getIntroductions()
+        model.addAttribute("introductions", introductions)
+        val links = service.getLinks()
+        model.addAttribute("links", links)
+        return "presentation/index"
+    }
+    @GetMapping("/resume")
+    fun resume(model: Model): String {
+        val resume = service.getResume()
+        model.addAttribute("resume", resume)
+        model.addAttribute("skillTypes", SkillType.values())
+        return "presentation/resume"
+    }
+    @GetMapping("/projects")
+    fun projects(model: Model): String {
+        val projects = service.getProjects()
+        model.addAttribute("projects", projects)
+        return "presentation/projects"
     }
 }
